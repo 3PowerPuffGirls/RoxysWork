d8ff23427c4a2f44c9836f21afea0ca88e978773
stackedwaffles88 <83725094+stackedwaffles88@users.noreply.github.com>
2021-06-23 13:05:14 -0400

    Update UploadImages.xaml.cs

--- UploadImages.xaml.cs
+++ UploadImages.xaml.cs
@@ -1,4 +1,4 @@
-﻿using MyPhotoLibraryApp_practice_.Model;
+using MyPhotoLibraryApp_practice_.Model;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -22,64 +22,22 @@ using Windows.UI.Xaml.Navigation;
 
 namespace MyPhotoLibraryApp_practice_
 {
-    
+
     public sealed partial class UploadImages : Page
     {
+        public ObservableCollection<Photo> photos { get; set; }
         public UploadImages()
         {
             this.InitializeComponent();
-        }
-
-        private void BackButton_Click(object sender, RoutedEventArgs e)
-        {
-            this.Frame.Navigate(typeof(MainPage));
-        }
-
-        private async void OpenFile_Click(object sender, RoutedEventArgs e)
-        {
-
-            var picker = new Windows.Storage.Pickers.FileOpenPicker();
-            picker.ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail;
-            picker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.PicturesLibrary;
-            picker.FileTypeFilter.Add(".jpg");
-            picker.FileTypeFilter.Add(".jpeg");
-            picker.FileTypeFilter.Add(".png");
-
-            Windows.Storage.StorageFile file = await picker.PickSingleFileAsync();
-            if (file != null)
-            {
-                // Application now has read/write access to the picked file
-                this.FileTextBlock.Text = "Picked photo: " + file.Name;
-                using (IRandomAccessStream fileStream = await file.OpenAsync(Windows.Storage.FileAccessMode.Read))
-                {
-                    // Set the image source to the selected bitmap
-                    BitmapImage bitmapImage = new BitmapImage();
-                    await bitmapImage.SetSourceAsync(fileStream);
-                    UploadImageArea.Source = bitmapImage;
-                    var addImage = PhotoManager.GetPhotos();
-                    string imageName = file.Name;
-                    addImage.Add(new Photo(imageName, PhotoCategory.New));
-                }
-
-            }
-            else
-            {
-                this.FileTextBlock.Text = "Operation cancelled.";
-            }
-        }
-
-        public void UpdateCollection()
-        {
-            
-        }
-        private void NewAlbumButton_Click(object sender, RoutedEventArgs e)
-        {
+            photos = new ObservableCollection<Photo>();
+            this.Loaded += UploadImages_Loaded;
 
         }
 
-        private void AlbumAddButton_Click(object sender, RoutedEventArgs e)
+        private async void UploadImages_Loaded(object sender, RoutedEventArgs e)
         {
-
+            await PhotoManager.GetAllPhotos(photos);
         }
     }
+
 }

6963516f17565a237e8615609ced3ba01809b5b0
stackedwaffles88 <83725094+stackedwaffles88@users.noreply.github.com>
2021-06-23 13:07:00 -0400

    Update Photo.cs

--- Photo.cs
+++ Photo.cs
@@ -1,17 +1,25 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 
-namespace MyPhotoLibraryApp_practice_
+namespace MyPhotoLibraryApp_practice_.Model
 {
     public class Photo
     {
         public string PhotoName { get; set; }
         public string PhotoFile { get; set; }
-        public string PhotoThumbnail { get; set; }
+        public ImageSource PhotoThumbnail { get; set; }
+        public ImageSource image { get; set; }
+        public StorageFile storagefile { get; set; }
+
+        public BitmapImage thumbnailbitmap { get; set; }
+
     }
 
 }
